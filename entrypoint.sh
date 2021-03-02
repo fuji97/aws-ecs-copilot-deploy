ffccd8de9ce52cdc4c434767f89c7330fedbd8c7
Federico 'fuji97' Rapetti <fuji1097@gmail.com>
2021-03-02 10:56:54 +0100

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+
+echo "Installing Docker"
+amazon-linux-extras install docker=18
+
+echo "cd into $GITHUB_WORKSPACE"
+cd $GITHUB_WORKSPACE
+
+# Download the copilot linux binary.
+echo "Duwnload the copilot linux binary"
+wget https://ecs-cli-v2-release.s3.amazonaws.com/copilot-linux-v1.2.0
+mv ./copilot-linux-v1.2.0 ./copilot-linux
+chmod +x ./copilot-linux
+
+# TODO Do login
+
+# Do deploy
+echo "Starting deploy"
+for env in $(INPUT_ENVIRONMENTS)
+do
+    for service in $(INPUT_SERVICES)
+    do
+        # TODO Add optional app name
+        echo "Deploying ${env} - ${service}"
+        ./copilot-linux deploy --env $env --name $service
+    done
+done
