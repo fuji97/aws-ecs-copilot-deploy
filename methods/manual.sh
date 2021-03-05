51565325b4d1c02a53c6286079e184987e51e5b2
Federico 'fuji97' Rapetti <fuji1097@gmail.com>
2021-03-05 10:59:21 +0100

    Improve template deploy fail detection

--- methods/manual.sh
+++ methods/manual.sh
@@ -161,7 +161,9 @@ for env in $INPUT_ENVIRONMENTS; do
             --capabilities CAPABILITY_NAMED_IAM \
             --s3-bucket "$s3_bucket" \
             --role-arn "$role")
-        if ! $res ; then
+        if $res; then
+            echo "Stack $stack deployed successfully"
+        else
             echo "::error::❌ Stack '$stack' deploy failed"
             exit 1
         fi
